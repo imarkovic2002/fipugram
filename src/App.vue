72790461df956d0d6b180200612b9bcfbcb2ca58
Administrator <markovici089@gmail.com>
2023-11-03 14:02:43 +0100

    Moja prva web stranica.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,86 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
-</template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
+    <div id="app">
+        <nav id="nav" class="navbar navbar-expand-md navbar-light">
+            <router-link to="/" class="navbar-brand">
+                <img src="@/assets/tfpu_logo.png" height="100" class="d-inline-block align-top" alt="" loading="lazy" />
+            </router-link>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarToggler"
+                aria-controls="navbarToggler"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarToggler">
+                <form id="search" class="navbar-form form-inline ml-auto">
+                    <input
+                        v-model="store.searchTerm"
+                        class="form-control mr-sm-2"
+                        type="search"
+                        placeholder="Pretraga"
+                        aria-label="Search"
+                    />
+                </form>
+                <!-- Image and text -->
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <router-link to="/login" class="nav-link">Login</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/signup" class="nav-link">Sign up</router-link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+  
+        <div class="container">
+            <router-view />
+            <!-- tu router dinamicki ubaci screen -->
+        </div>
+    </div>
+  </template>
+  
+  <script>
+  import store from '@/store';
+  
+  export default {
+    name: 'app',
+    data() {
+        return {
+            store,
+        };
+    },
+  };
+  </script>
+  
+  <style lang="scss">
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+  }
+  
+  #nav {
+    padding: 30px;
+  
+    a {
+        font-weight: bold;
+        color: #2c3e50;
+  
+        &.router-link-exact-active {
+            color: #42b983;
+        }
     }
   }
-}
-</style>
+  
+  #search {
+    display: block;
+    text-align: center;
+  }
+  </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+    <div class="row">
+        <div class="col-8">
+            <instagram-card v-for="card in filteredCards" :key="card.url" :info="card" />
+        </div>
+        <div class="col-4">
+            Sidebar
+        </div>
+    </div>
+  </template>
+  <script>
+  // @ is an alias to /src
+  import InstagramCard from '@/components/InstagramCard.vue';
+  import store from '@/store';
+  
+  let cards = [];
+  
+  //... API/Firebase -> sve kartice -> cards
+  
+  cards = [
+    { url: 'https://picsum.photos/id/1/400/400', description: 'laptop', time: 'few minutes ago...' },
+    { url: 'https://picsum.photos/id/2/400/400', description: 'laptop #2', time: 'hour ago...' },
+    { url: 'https://picsum.photos/id/3/400/400', description: 'laptop #3', time: 'few hours ago...' },
+  ];
+  
+  export default {
+    name: 'home',
+    data: function() {
+        return {
+            cards,
+            store,
+        };
+    },
+    computed: {
+        filteredCards() {
+            // logika koja filtrira cards
+            let termin = this.store.searchTerm;
+  
+            return this.cards.filter((card) => card.description.includes(termin));
+        },
+    },
+    components: {
+        InstagramCard,
+    },
+  };
+  </script>
